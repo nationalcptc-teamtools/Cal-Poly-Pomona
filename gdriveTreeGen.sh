58237a89b446f6820f036692bb77e19adefe6618
toshniko <9406675+toshniko@users.noreply.github.com>
2019-10-11 14:53:18 +0100

    First Commit

---
+++ gdriveTreeGen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#  ____       __  __       _
+# / ___|     |  \/  | __ _| | _____ _ __
+#| |  _ _____| |\/| |/ _` | |/ / _ \ '__|
+#| |_| |_____| |  | | (_| |   <  __/ |
+# \____|     |_|  |_|\__,_|_|\_\___|_|
+ 
+# Create directory structure for G Drive
+
+names=(
+	Angelo
+	David
+	Andy
+	Joseph
+	Dennis
+	Silas
+)
+list=(
+	Scans
+	EyeWitness
+	Notes
+	Cracked
+	Uncracked
+)
+for i in "${names[@]}"; do
+	for x in "${list[@]}"; do
+		gdrive mkdir "$i-$x"
+done
+done
+
+
