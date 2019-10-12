410cd28e646ddc23ab2fc4c3f9951844177826fa
toshniko <9406675+toshniko@users.noreply.github.com>
2019-10-11 22:11:45 -0700

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,20 +1,28 @@
-:: Install choco .exe and add choco to PATH
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+$fireeyeFeed      = "https://www.myget.org/F/fireeye/api/v2"
+$ErrorActionPreference = 'Continue'
 
-:: Install all the packages
-choco install mobaxterm -fy
-choco install googlechrome -fy
-choco install firefox -fy
-choco install jre8 -fy
-choco install zap -fy
-choco install winscp -fy
-choco install vlc -fy
-choco install 7zip.install -fy
-choco install wireshark -fy
-choco install python3 --pre -fy
-choco install python2 -fy
-choco install nmap -fy
-choco install sublimetext3 -fy
-choco install greenshot -fy
-choco install git.install -fy
-choco install burp-suite-free-edition -fy
+function InitialSetup {
+	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	# Chocolatey setup
+	Write-Host "Initializing chocolatey"
+	$toolListDirShortcut = Join-Path ${Env:UserProfile} "Desktop\Tools.lnk"
+	[Environment]::SetEnvironmentVariable("TOOL_LIST_SHORTCUT", $toolListDirShortcut, 2)
+	iex "choco sources add -n=fireeye -s $fireeyeFeed --priority 1"
+	iex "choco feature enable -n allowGlobalConfirmation"
+	iex "choco feature enable -n allowEmptyChecksums"
+}
+function MiscSetup {
+	#disable windows defender
+	sc config WinDefend start= disabled
+	sc stop WinDefend
+
+}
+
+function Main {
+	InitialSetup
+	MiscSetup
+cinst common.fireeye
+cinst .\packages.config
+}
+
+Main
