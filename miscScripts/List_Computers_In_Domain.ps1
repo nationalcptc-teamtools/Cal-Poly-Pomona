4db95e7d0b35dc2b4feedef590c2785330a2170e
Andy Chiang <9406675+toshtato@users.noreply.github.com>
2021-01-05 14:18:39 -0800

    only FindMediaFiles

--- miscScripts/List_Computers_In_Domain.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module ActiveDirectory 
-Get-ADComputer -Filter * -properties *|select Name, DNSHostName, OperatingSystem, LastLogonDate -Verbose
-
-
-
-
-
-#Keep PowerShell Console Window Open After Script Finishes Running
-Read-Host -Prompt "Press Enter to exit"
