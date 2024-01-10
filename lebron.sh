5b6ee92aca032e39e370fd77e181c5b630e33275
Dylan Tran <68256613+susMdT@users.noreply.github.com>
2024-01-10 11:49:13 -0800

    i was lightning before the thunder (thunder)
    landin the cut

---
+++ lebron.sh
@@ -0,0 +1,6 @@
+# i got one more left in me
+# he was the lightning before the thunder
+echo "set-option -g history-limit 20000" >> ~/.tmux.conf
+
+echo "autoload -U colors && colors" >> ~/.zshrc
+echo "export PROMPT=\$PROMPT'%{\$fg[yellow]%}[%D{%m/%f/%y} %D{%L:%M:%S}] '" >> ~/.zshrc 
